bc5b8d1e970c81f7c2bf57916dba243048e061f9
bvpav <78971592+bvpav@users.noreply.github.com>
2025-05-03 08:29:34 +0300

    added geist font

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,3 +1,5 @@
+import "@fontsource-variable/geist";
+
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
@@ -7,9 +9,9 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-    <React.StrictMode>
-        <QueryClientProvider client={queryClient}>
-            <App />
-        </QueryClientProvider>
-    </React.StrictMode>
+  <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
+  </React.StrictMode>
 );
